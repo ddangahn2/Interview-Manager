d55f1c2555005ea6bb963aecbdf6fc4452242d31
Sangahn <sangahn0524@gmail.com>
2024-11-29 18:10:16 +0900

    initial

---
+++ src/test/java/com/interviewapp/manager/InterviewManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interviewapp.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
