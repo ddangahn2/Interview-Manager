e0079af41af9c49d85ea82257ed1597186609e50
Sangahn <sangahn0524@gmail.com>
2024-12-16 23:08:34 +0900

    feat: webm to mp4

--- src/main/java/com/interviewapp/manager/controller/VideoUploadController.java
+++ src/main/java/com/interviewapp/manager/controller/VideoUploadController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import ws.schild.jave.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -20,8 +21,15 @@ public class VideoUploadController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("파일이 비어있습니다");
         }
         try{
-            File destination = new File(UPLOAD_DIR + file.getOriginalFilename());
-            file.transferTo(destination);
+            String originalFileName = file.getOriginalFilename();
+            File sourceFile = new File(UPLOAD_DIR + originalFileName);
+            file.transferTo(sourceFile);
+
+            String mp4FileName = originalFileName.substring(0, originalFileName.lastIndexOf('.')) + ".mp4";
+            File destination = new File(UPLOAD_DIR + mp4FileName);
+
+            // 변환
+            convertToMp4(sourceFile, destination);
 
             return ResponseEntity.status(HttpStatus.OK).body("파일 업로드 성공");
         } catch (IOException e) {
